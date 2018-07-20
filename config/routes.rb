7209a4c48258efb48356383c3caddbeb1e17cc1a
Andrés F. López A <ing.ratosocial@gmail.com>
2018-07-20 14:10:50 -0500

    blog post with Ajax :D

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+  resources :posts do
+    resources :comments
+  end
   resources :comments
-  resources :posts
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
